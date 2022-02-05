0ae3b16010fcba10fb432af5afb9f1bd7710a45b
MRG-07 <76522866+MRG-07@users.noreply.github.com>
2022-02-05 23:29:47 +0700

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -1,7 +1,7 @@
 let fetch = require('node-fetch')
 
 let timeout = 120000
-let poin = 500
+let poin = 6000000
 let src
 let handler = async (m, { conn, usedPrefix }) => {
     conn.caklontong = conn.caklontong ? conn.caklontong : {}
